0ede28cba50d4803918d87249f7532cac7a6a5ca
Krystian Radziszewski <krystian-radziszewski-it@o2.pl>
2023-09-26 11:13:40 +0200

    offers edit

--- components/Cards/CardOffer.tsx
+++ components/Cards/CardOffer.tsx
@@ -13,7 +13,7 @@ const CardOffer: React.FC<CardOfferProps> = ({ icon, title, description }) => {
 		<div className="  text-slate-100 font-myfont mb-4">
 			<div className="flex flex-col justify-center items-center ">
 				<SelectIcon size={40} className="text-[#c49d39] mb-1" />
-				<h2 className=" mb-6 tracking-wider text-xl text-[#f4d073d2]">{title}</h2>
+				<h2 className="text-center mb-6 tracking-wider text-xl text-[#f4d073d2]">{title}</h2>
 			</div>
 			<div className=" text-center tracking-wide">{description}</div>
 		</div>
